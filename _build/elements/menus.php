5f921ea4a83b30a7531205269d4a00725daf1385
Bannikov Andrey <vectorserver@mail.ru>
2022-06-06 01:41:05 +0500

    Add files via upload

---
+++ _build/elements/menus.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Remove Duplicates MODX' => [
+        'description' => 'Чекер дубликатов!',
+        'action' => 'index',
+        'namespace' => 'remove_duplicates_modx',
+        //'icon' => '<i class="icon icon-large icon-modx"></i>',
+    ],
+];
